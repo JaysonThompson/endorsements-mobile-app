2dcd87b92b6b6764fe2803319e28d5dec9b7a7b6
Jayson Thompson <jaysonthompson922@gmail.com>
2023-06-04 22:32:00 -0700

    added functionality to get "to" and "from" from realtime database

--- index.js
+++ index.js
@@ -9,8 +9,6 @@ const appSettings = {
 const app = initializeApp(appSettings)
 const database = getDatabase(app)
 const commentsInDB =  ref(database, "comments")
-const recipientInDB = ref(database, "comments/recipient")
-console.log(recipientInDB)
 const inputFieldEl = document.querySelector("#text-input")
 const submitBtnEl = document.querySelector("#publish-btn")
 const commentContainerEl = document.querySelector("#comments-container")
@@ -23,8 +21,11 @@ onValue(commentsInDB, snapshot => {
         let arrayItems = Object.entries(snapshot.val())
         for(let i = 0; i < arrayItems.length; i++) {
             let currentItem = arrayItems[i]
-            addNewComment(currentItemValue)
-            //console.log(currentItem)
+            const [ id, commentData ] = currentItem
+            const comment = commentData.comment
+            const sender = commentData.sender
+            const recipient = commentData.recipient
+            addNewComment(recipient, comment, sender)
         }
     } else {
         commentContainerEl.innerHTML = "There are no comments at this time."
@@ -43,9 +44,13 @@ submitBtnEl.addEventListener('click', () => {
 
 const addCommentToDB = obj => push(commentsInDB, obj)
 
-const addNewComment = text =>
+const addNewComment = (recipient, text, sender) =>
     commentContainerEl.innerHTML += `
-    <section class="comment"><p>${text}</p></section
+    <section class="comment">
+    <p class="recipient">To: ${recipient}</p>
+    <p>${text}</p>
+    <p class="sender">From: ${sender}</p>
+    </section
     `
 
 const clearComments = () => commentContainerEl.innerHTML = ""
